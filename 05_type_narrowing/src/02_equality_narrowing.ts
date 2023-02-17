8245eaaa8a85f81ecfc834fb75342478006f959d
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-02-17 10:29:44 +0900

    02_equality_narrowing.ts

--- 05_type_narrowing/src/02_equality_narrowing.ts
+++ 05_type_narrowing/src/02_equality_narrowing.ts
@@ -1,9 +1,12 @@
 function doSomething(x: string | number, y: string | boolean) {
-  if (x == y) {
+  if (x === y) {
+    // x = y ということは、xはstring型。
     x.trim();
   }
 
-  if (x === y) {
-    x;
+  if (x !== y) {
+    // X != y ということは、xは string | number。
+    // Property 'trim' does not exist on type 'number'.
+    x.trim();
   }
 }
